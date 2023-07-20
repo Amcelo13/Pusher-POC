6b5292d8dadad7562790ade66e2e6597ad6fec50
Chetan Thakur <80780935+Amcelo13@users.noreply.github.com>
2023-07-20 22:19:47 +0530

    Y

--- pusherapp/src/App.jsx
+++ pusherapp/src/App.jsx
@@ -1,28 +1,28 @@
-import React, {  useEffect, useState } from 'react';
-import axios from 'axios';
-import Pusher from 'pusher-js';
-import ChatList from './ChatList';
-import ChatBox from './ChatBox';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import Pusher from "pusher-js";
+import ChatList from "./ChatList";
+import ChatBox from "./ChatBox";
+import "./App.css";
 
 const App = () => {
   const [text, setText] = useState("");
-  const username='Deepak';
+  const username = "Chetan";
   const [chat, setChat] = useState([]);
-  const [msg, setMsg] = useState()
+  const [msg, setMsg] = useState();
 
   useEffect(() => {
-    const pusher = new Pusher('d880ba05057d516534d1', {
-      cluster: 'ap2',
+    const pusher = new Pusher("d880ba05057d516534d1", {
+      cluster: "ap2",
       encrypted: true,
     });
 
     const channel = pusher.subscribe("Systumm");
     channel.bind("message", (data) => {
-      console.log(data)
+      console.log(data);
       setMsg(data);
-    })
-    
+    });
+
     return () => {
       pusher.unsubscribe("Systumm");
     };
@@ -36,23 +36,26 @@ const App = () => {
     if (e.keyCode === 13) {
       const payload = {
         username: username,
-        message: text
+        message: text,
       };
       try {
-        await axios.post('http://localhost:4000/message', payload);
-        setText('');
+        await axios.post("http://localhost:4000/message", payload);
+        setText("");
       } catch (error) {
-        console.log(error)
+        console.log(error);
       }
-
     } else {
       setText(e.target.value);
     }
-  }
+  };
   return (
     <div className="App">
       <header className="App-header">
-        <img src={""} className="App-logo" alt="logo" />
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/8/8e/Nextjs-logo.svg"
+          className="App-logo"
+          alt="logo"
+        />
         <h1 className="App-title">Welcome to React-Pusher Chat</h1>
       </header>
       <section>
@@ -66,7 +69,6 @@ const App = () => {
       </section>
     </div>
   );
+};
 
-}
-
-export default App
+export default App;
